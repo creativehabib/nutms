15b6e95b0d0bf07c48e58889855e417d8f9f0ae2
Habibur Rahaman <shamolrahaman@gmail.com>
2026-07-25 11:34:43 +0600

    Clear checkbox state after bulk actions

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Teacher extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     // কোনো কলামই প্রোটেক্টেড নয়, সব কলামে ডেটা ইনসার্ট করা যাবে
     protected $guarded = [];
