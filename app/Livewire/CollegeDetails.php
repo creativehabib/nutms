df1c0c51604fc5ef02a44af04bea7547626657a8
Habibur Rahaman <shamolrahaman@gmail.com>
2026-07-31 00:47:57 +0600

    Restore training history in teacher profiles

---
+++ app/Livewire/CollegeDetails.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\College;
+use Illuminate\Contracts\View\View;
+use Livewire\Attributes\Locked;
+use Livewire\Component;
+
+class CollegeDetails extends Component
+{
+    #[Locked]
+    public College $college;
+
+    public function mount(College $college): void
+    {
+        $this->college = $college->load(['division:id,name,bn_name', 'district:id,name,bn_name', 'thana:id,name,bn_name', 'programs'])
+            ->loadCount('teachers');
+    }
+
+    public function render(): View
+    {
+        return view('livewire.college-details');
+    }
+}
