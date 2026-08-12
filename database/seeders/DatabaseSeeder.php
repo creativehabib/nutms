694b49f0bce4b76ec9c7e0246651fdf8803a8345
creativehabib <shamolrahaman@gmail.com>
2026-08-12 11:33:44 +0600

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,7 @@ class DatabaseSeeder extends Seeder
             ThanaSeeder::class,
             UnionSeeder::class,
             AffiliatedCollegeSeeder::class,
+            SubjectSeeder::class,
             ProgramLevelSeeder::class,
         ]);
 
